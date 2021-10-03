f39e6b19483abb041f350f19f9aa302988af1890
S-Suchyy <sucheth.seethalla@gmail.com>
2021-10-02 21:28:27 -0400

    Commit 1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FFTeleOp.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "FFTeleOp", group = "TeleOp")
+public class FFTeleOp extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+    //TEST ONE
+
+
+}
