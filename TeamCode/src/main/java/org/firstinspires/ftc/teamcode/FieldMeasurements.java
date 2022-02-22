93c3479e856159fe4dfb68ff6f3126ae0dbe5495
Robotics <sucheth.seethalla@gmail.com>
2022-02-22 15:02:01 -0500

    k

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldMeasurements.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldMeasurements.java
@@ -3,8 +3,14 @@ package org.firstinspires.ftc.teamcode;
 
 public class FieldMeasurements {
     public static final double TILE_WIDTH = 610; //3728.44037 in encoder ticks //2.15 per two tiles
-    //1.0.75 for 1 tile  at speed 0.5
-    //1440
+
+    private static final int ticksPerRotation = 1440;
+    private static final int diameter = 100; //in mm
+    //2 feet = 609.6mm, length of tile
+    //diameter * 3.14 = C, 609.6mm*1440 ticks/C (mm) = ticks per tile
+
+    private static final double ticksPerTile  = (609.6 * 1440) / (diameter * 3.14);
+
     public static final long TIME_FOR_TILE = 1075; //in MS
     private static final int STAGEONE_HEIGHT = 3300;    //IN TICKS
     private static final int STAGETWO_HEIGHT = 5300;   //IN TICKS
@@ -22,5 +28,7 @@ public class FieldMeasurements {
     public static int getStageThreeHeight() {
         return STAGETHREE_HEIGHT;
     }
+
+    public static int getTicksPerTile() { return (int)ticksPerTile; }
 
 }
